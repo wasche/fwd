09611ec9762b52438d9a487de6227703f3903b61
Wil Asche <wil@asche.us>
2015-12-14 16:49:34 -0500

    fixes #13, #9

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+  , Forward = mongoose.model('Forward')
+  ;
+
+exports.path = '/_admin';
+
+exports.get = function(req, res){
+  Forward
+  .find({})
+  .sort({name:1})
+  .exec(function(err, docs){
+    res.render('admin', {
+      title: null
+    , forwards: docs
+    });
+  });
+};
