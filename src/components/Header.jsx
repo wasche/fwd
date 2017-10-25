5ba4a358f7568e7a7a2ea4bb025ae3c3b5bf3204
Wil Asche <wil@asche.us>
2017-10-25 11:38:57 -0400

    start on search screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,22 @@ export default class Header extends Component {
   render () {
     return (
       <header className='hero-head'>
-        <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <nav className='navbar is-transparent' role='navigation' aria-label='main navigation'>
           <div className='navbar-brand'>
             <div className='navbar-item'>fwd</div>
           </div>
-          <div className='navbar-menu is-active'>
+          <div className='navbar-menu'>
             <div className='navbar-start' />
             <div className='navbar-end'>
-              <Link to='/' exact className='navbar-item' activeClassName='is-active'>Home</Link>
-              <Link to='/add' exact className='navbar-item' activeClassName='is-active'>Add</Link>
+              <Link to='/' exact className='navbar-item' activeClassName='is-active'>
+                <span className='icon'><i className='fa fa-home' /></span>
+              </Link>
+              <Link to='/add' exact className='navbar-item' activeClassName='is-active'>
+                <span className='icon'><i className='fa fa-plus' /></span>
+              </Link>
+              <Link to='/search' exact className='navbar-item' activeClassName='is-active'>
+                <span className='icon'><i className='fa fa-search' /></span>
+              </Link>
             </div>
           </div>
         </nav>
